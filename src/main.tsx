153c85c878d81ebd34a2d135a86e40b82c087f9f
sarahrichfit23-hue <sarahrichfit23@gmail.com>
2025-10-06 03:24:18 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-https://chatgpt.com/c/68e215fd-9034-832c-9933-59a5de5b9d1b
+import ErrorBoundary from './ErrorBoundary'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <ErrorBoundary>
     <App />
   </ErrorBoundary>
-);
+)
