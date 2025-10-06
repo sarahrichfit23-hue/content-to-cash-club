e9e64ddf67e8cbcc89b045270d4ccef6243b83b0
sarahrichfit23-hue <sarahrichfit23@gmail.com>
2025-10-05 23:45:17 -0700

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
+import ErrorBoundary from './ErrorBoundary.tsx'
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
