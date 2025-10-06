e5b5cdb7b4c432bd2325b1ccbd30937274277acc
sarahrichfit23-hue <sarahrichfit23@gmail.com>
2025-10-06 03:04:54 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
+https://chatgpt.com/c/68e215fd-9034-832c-9933-59a5de5b9d1b
 import './index.css'
 
 createRoot(document.getElementById("root")!).render(
