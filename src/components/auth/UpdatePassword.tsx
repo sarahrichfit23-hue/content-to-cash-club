9d1b64f550ca3e6cc36bf220a34ad044709a8ac7
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-07 12:18:00 -0700

    Add missing UpdatePassword component

---
+++ src/components/auth/UpdatePassword.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function UpdatePassword() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Update Password</h1>
+      <p>This is a placeholder page for updating passwords. You can customize it later.</p>
+    </div>
+  );
+}
