9e986317833f6023badd153ea2be5f248988d697
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-07 15:49:25 -0700

    Reinstalled all ShadCN UI components cleanly (fixed template issues)

--- src/components/ui/sonner.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import * as React from "react";
-
-export interface SonnerProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-export function Sonner(props: SonnerProps) {
-  return <div {...props} />;
-}
--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,8 +1,35 @@
-"use client";
-import * as React from "react";
+"use client"
 
-export interface ToasterProps extends React.HTMLAttributes<HTMLDivElement> {}
+import { useToast } from "@/hooks/use-toast"
+import {
+  Toast,
+  ToastClose,
+  ToastDescription,
+  ToastProvider,
+  ToastTitle,
+  ToastViewport,
+} from "@/components/ui/toast"
 
-export function Toaster(props: ToasterProps) {
-  return <div {...props} />;
+export function Toaster() {
+  const { toasts } = useToast()
+
+  return (
+    <ToastProvider>
+      {toasts.map(function ({ id, title, description, action, ...props }) {
+        return (
+          <Toast key={id} {...props}>
+            <div className="grid gap-1">
+              {title && <ToastTitle>{title}</ToastTitle>}
+              {description && (
+                <ToastDescription>{description}</ToastDescription>
+              )}
+            </div>
+            {action}
+            <ToastClose />
+          </Toast>
+        )
+      })}
+      <ToastViewport />
+    </ToastProvider>
+  )
 }
