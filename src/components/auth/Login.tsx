a4cf589efe0a38ed3f51dd28ac8c45b419f3b52a
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-07 12:13:29 -0700

    Add missing Login component

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function Login() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Login Page</h1>
+      <p>This is a placeholder login component. You can customize it later.</p>
+    </div>
+  );
+}
