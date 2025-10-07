0255b8d63f802adf6fd488a3f39b8426f2d8f66e
sarahrichfit23-hue <sarahrichfit23@gmail.com>
2025-10-07 02:08:29 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
 export default defineConfig({
-  root: "src",
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+});
+
   },
   build: {
     outDir: "../dist",
