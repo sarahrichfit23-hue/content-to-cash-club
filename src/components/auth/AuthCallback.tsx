c850601614e32d88f5e4f194d9a9d421e290630e
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-07 12:20:36 -0700

    Add missing AuthCallback component

---
+++ src/components/auth/AuthCallback.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function AuthCallback() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Authenticating...</h1>
+      <p>Please wait while we complete your login.</p>
+    </div>
+  );
+}
