90de4f49485e5bf4815421613db60cc899e0db72
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-07 12:15:35 -0700

    Add missing ResetPassword component

---
+++ src/components/auth/ResetPassword.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function ResetPassword() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Reset Password</h1>
+      <p>This is a placeholder Reset Password page. You can add real logic later.</p>
+    </div>
+  );
+}
