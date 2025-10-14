215b018b96b6be98d69c9734e340bdc66211bf11
Sarah Richardson <sarahrichardson@Sarahs-MacBook-Pro.local>
2025-10-13 23:27:21 -0700

    Fix: remove stray npm reference in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
+import './index.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthProvider";
-import './index.css';
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,6 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
-);npm
+);
 
 
